8099434bc8f23fd7ad6410d950f282f70a7e00dd
daniel <dg189948@gmail.com>
2024-01-18 09:01:36 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+import http from 'http';
+const PORT = 3000;
+
+const rotas = {
+    "/": "Daniel'",
+}
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(rotas[req.url] || "rota não encontrada");
+});
+
+server.listen(PORT, () => {
+    console.log('Server running at http://localhost:3000/');
+});
